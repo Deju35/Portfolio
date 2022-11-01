24ff5419836169763479b87771215e9b1e559c12
Dejen Tesfai <107037555+Deju35@users.noreply.github.com>
2022-11-01 18:46:46 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -62,27 +62,27 @@ const Navbar = () => {
         <ul className={!nav ?
            'hidden':
            ' absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-          <li className='py-6 text-4xl'>
+          <li className='py-6 text-4xl text-pink-600'>
           <Link onClick={handleClick} to='home' smooth={true} duration={500}>
             Home
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl text-pink-600'>
           <Link onClick={handleClick} to='portfolio' smooth={true} duration={500}>
             Portfolio
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl text-pink-600'>
           <Link onClick={handleClick} to='about' smooth={true} duration={500}>
             About
           </Link>
         </li>
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl text-pink-600'>
           <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
             Skills
           </Link>
         </li> 
-        <li className='py-6 text-4xl'>
+        <li className='py-6 text-4xl text-pink-600'>
           <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
             Contact
           </Link>
